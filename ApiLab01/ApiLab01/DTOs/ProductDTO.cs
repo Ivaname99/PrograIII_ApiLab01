69216a20d0bf3c4e55b8a1ff47fb54e892755684
Ivaname99 <mg0913032023@unab.edu.sv>
2025-02-19 13:00:52 -0600

    Restauracion del proyecto en un nuevo repositorio

---
+++ ApiLab01/ApiLab01/DTOs/ProductDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiLab01.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
